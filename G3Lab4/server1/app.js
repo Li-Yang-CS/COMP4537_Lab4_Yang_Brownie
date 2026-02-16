20db5b59ff93498da2ab772c30299151f4c6bc01
Li-Yang-CS <kavenli23@gmail.com>
2026-02-15 23:33:58 -0800

    spereated the js locig

---
+++ G3Lab4/server1/app.js
@@ -0,0 +1,33 @@
+        // Strings for client side
+        const CLIENT_STRINGS = {
+            url_insert: "http://localhost:8888/api/insert",
+            url_query: "http://localhost:8888/api/query?sql=",
+            err_network: "Network Error: "
+        };
+
+        // A. Handle Insert Button
+        document.getElementById('insertBtn').addEventListener('click', () => {
+            fetch(CLIENT_STRINGS.url_insert, { method: 'POST' })
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('insertFeedback').innerText = data;
+                })
+                .catch(err => {
+                    document.getElementById('insertFeedback').innerText = CLIENT_STRINGS.err_network + err;
+                });
+        });
+
+        // B. Handle Query Button
+        document.getElementById('submitQueryBtn').addEventListener('click', () => {
+            const query = document.getElementById('sqlQuery').value;
+            const url = CLIENT_STRINGS.url_query + encodeURIComponent(query);
+            
+            fetch(url, { method: 'GET' })
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('queryFeedback').innerText = data;
+                })
+                .catch(err => {
+                    document.getElementById('queryFeedback').innerText = CLIENT_STRINGS.err_network + err;
+                });
+        });
